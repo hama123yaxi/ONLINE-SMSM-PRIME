47b78ef42c8daa1beed948893a375a557a9a43e7
hama123yaxi <92339868+hama123yaxi@users.noreply.github.com>
2021-10-12 17:35:56 +0300

    Update smsm.js

--- smsm.js
+++ smsm.js
@@ -2935,7 +2935,7 @@ smsm120.on("ready", () => {
 
 //////////////// token lera dabne/////////
 
-smsm.login("");
+smsm.login("ODk3MjQ2MzYyNDY1MzQ1NTY3.YWWYgA.J_Y74htaVCjK5hUrkvxwDNDBUzI");
 smsm2.login("");
 smsm3.login("");
 smsm4.login("");
